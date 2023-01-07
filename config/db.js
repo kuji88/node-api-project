32a2eca80531e62af91d315bb8f91974a584d13c
kuji <kuji99@aol.com>
2023-01-07 19:28:26 +0300

    Update

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 
-const dbConnection =()=>{
+const dbConnection = async()=>{
     try {
-        mongoose.set('strictQuery', false)
+       await mongoose.set('strictQuery', false)
         mongoose.connect(process.env.mongodb_URL)
         .then(()=>console.log("connection done"))
         .catch((err)=> console.log("connection lost",err))
